6fc33ae02d8f8307b222869791abb573ae848e46
epoxitwork <epoxitwork@gmail.com>
2017-10-26 16:13:54 +0300

    Task_03 added

---
+++ Task_03/Task_03/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+using OpenQA.Selenium;
+
+namespace Task_03
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+            driver = new ChromeDriver();
+            driver.Manage().Window.Position = new Point(0, 0);
+            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(120);
+            driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin/login.php");
+            string locUserName = "//input[@name='username']";
+            string locPassword = "//input[@name='password']";
+            string txtCredential = "admin";
+            string locLoginBtn = "//button";
+            WaitForLoading(driver, locUserName);
+            driver.FindElement(By.XPath(locUserName)).SendKeys(txtCredential);
+            driver.FindElement(By.XPath(locPassword)).SendKeys(txtCredential);
+            driver.FindElement(By.XPath(locLoginBtn)).Click();
+            driver.Quit();
+        }
+        private static void WaitForLoading(IWebDriver driver, string target)
+        {
+            while (!IsElementPresent(driver, target))
+            {
+                Thread.Sleep(50);
+            }
+        }
+
+        private static bool IsElementPresent(IWebDriver driver, string target)
+        {
+            try
+            {
+                driver.FindElement(By.XPath(target));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+    }
+}
