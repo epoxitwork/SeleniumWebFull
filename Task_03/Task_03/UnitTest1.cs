1f32733e248a5fe08db96e8477a1d7e8c69b3789
epoxitwork <epoxitwork@gmail.com>
2017-10-26 16:18:35 +0300

    Task_03 upd

--- Task_03/Task_03/UnitTest1.cs
+++ Task_03/Task_03/UnitTest1.cs
@@ -13,20 +13,32 @@ namespace Task_03
         [TestMethod]
         public void TestMethod1()
         {
+            /*.
+            Компоненты ставил через консоль так:
+            Install-Package NUnit -Version 2.6.4
+            Install-Package Selenium.WebDriver
+            Install-Package Selenium.Support
+            */
             IWebDriver driver;
             driver = new ChromeDriver();
-            driver.Manage().Window.Position = new Point(0, 0);
-            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(120);
-            driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin/login.php");
-            string locUserName = "//input[@name='username']";
-            string locPassword = "//input[@name='password']";
-            string txtCredential = "admin";
-            string locLoginBtn = "//button";
-            WaitForLoading(driver, locUserName);
-            driver.FindElement(By.XPath(locUserName)).SendKeys(txtCredential);
-            driver.FindElement(By.XPath(locPassword)).SendKeys(txtCredential);
-            driver.FindElement(By.XPath(locLoginBtn)).Click();
-            driver.Quit();
+            try
+            {
+                driver.Manage().Window.Position = new Point(0, 0);
+                driver.Manage().Timeouts().ImplicitWait = new TimeSpan(120);
+                driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin/login.php");
+                string locUserName = "//input[@name='username']";
+                string locPassword = "//input[@name='password']";
+                string txtCredential = "admin";
+                string locLoginBtn = "//button";
+                WaitForLoading(driver, locUserName);
+                driver.FindElement(By.XPath(locUserName)).SendKeys(txtCredential);
+                driver.FindElement(By.XPath(locPassword)).SendKeys(txtCredential);
+                driver.FindElement(By.XPath(locLoginBtn)).Click();
+            }
+            finally
+            {
+                driver.Quit();
+            }
         }
         private static void WaitForLoading(IWebDriver driver, string target)
         {
